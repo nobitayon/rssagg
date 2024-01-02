9421972f81512d9f55c676378bba62166a442af5
yonathanm10 <yonathanm@cocoindonesia.com>
2024-01-02 14:27:19 +0700

    build(handler_user):Add endpoint to get user from api_key. Also package auth to get api key from header

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/nobitayon/rssagg/internal/database"
+	"github.com/nobitayon/rssagg/internal/auth"
 )
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request){
@@ -31,5 +32,19 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		respondWithError(w,400,fmt.Sprintf("Couldn't create user:%s",err))
 		return
 	}
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request){
+	apiKey, err:=auth.GetAPIKey(r.Header)
+	if err!=nil{
+		respondWithError(w,403,fmt.Sprintf("Auth error:%s",err))
+		return 
+	}
+	user,err:=apiCfg.DB.GetUserByAPIKey(r.Context(),apiKey)
+	if err!=nil{
+		respondWithError(w,400,fmt.Sprintf("Couldn't get user:%v",err))
+		return
+	}
+	respondWithJSON(w,200,databaseUserToUser(user))
 }
